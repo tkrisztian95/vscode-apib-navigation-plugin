77be875629695e5b81360c79a79cf8edca7f51e0
Tóth Krisztián Gyula <t.krisztian95@gmail.com>
2021-07-06 11:54:18 +0200

    feat: Outline requests as method

--- extension.js
+++ extension.js
@@ -41,8 +41,12 @@ module.exports = {
  * 
  * # Node 1
  * 	## Node 2
+ *  + Request
+ *  + Request
  * 		### Node 3
+ *  + Request
  *	 		#### Node 4
+ *  + Request
  *	 		#### Node 4
  *      ### Node 3
  *      ### Node 3
@@ -54,26 +58,61 @@ var ApibConfigDocumentSymbolProvider = /** @class */ (function () {
 	function ApibConfigDocumentSymbolProvider() {
 	}
 
-	function getSymbolRank(symbol) {
-		return symbol.name.split(" ", 1)[0].length;
+	function isSectionSymbol(symbol) {
+		return getSectionRank(symbol) > 0;
 	}
 
-	function isRootSymbol(symbol) {
-		var rank = getSymbolRank(symbol);
-		return rank === 1;
+	function getSectionRank(symbol) {
+		const regex = /^#*/g;
+		const found = symbol.name.match(regex);
+		if (found == null) {
+			return 0;
+		}
+		return found[0].length;
 	}
 
-	function findChildNodes(parentIdx, symbols) {
+	function isRootSection(symbol) {
+		return getSectionRank(symbol) == 1;
+	}
+
+	function isRequest(text) {
+		return text.startsWith("+ Request")
+	}
+
+	function findNestedSections(parentIdx, symbols) {
+
+		var parentRank = getSectionRank(symbols[parentIdx]);
+
 		var nodes = [];
-		var parentRank = getSymbolRank(symbols[parentIdx]);
 		for (var j = parentIdx + 1; j < symbols.length; j++) {
-			var nextRank = getSymbolRank(symbols[j]);
+			var next = symbols[j];
+
+			if (!isSectionSymbol(next)) {
+				continue;
+			}
+
+			var nextRank = getSectionRank(next);
 			if (parentRank + 1 == nextRank) {
-				nodes.push(symbols[j]); //Node is a child
+				nodes.push(next); //Node is a child
+				continue;
 			}
 			if (nextRank <= parentRank) {
 				break; // No more child
 			}
+		}
+		return nodes;
+	}
+
+	function findRequests(parentIdx, symbols) {
+		var nodes = [];
+		for (var j = parentIdx + 1; j < symbols.length; j++) {
+			var next = symbols[j];
+
+			if (!isRequest(next.name)) {
+				return nodes;
+			}
+
+			nodes.push(next); //Node is a child request
 		}
 		return nodes;
 	}
@@ -85,31 +124,45 @@ var ApibConfigDocumentSymbolProvider = /** @class */ (function () {
 			for (var i = 0; i < document.lineCount; i++) {
 				var line = document.lineAt(i);
 				if (line.text.startsWith("#")) {
-					var symbol = new vscode.DocumentSymbol(line.text, 'Component', vscode.SymbolKind.Package, line.range, line.range);
-					
+					var section = new vscode.DocumentSymbol(line.text, 'Section', vscode.SymbolKind.Package, line.range, line.range);
+
 					if (line.text.includes("(object)")) {
-						symbol.kind = vscode.SymbolKind.Class;
+						section.detail = 'Named Type';
+						section.kind = vscode.SymbolKind.Class;
 					}
 
 					if (line.text.includes("(enum)")) {
-						symbol.kind = vscode.SymbolKind.Enum;
+						section.detail = 'Named Type';
+						section.kind = vscode.SymbolKind.Enum;
 					}
-					
-					symbols.push(symbol);
+					symbols.push(section);
+				}
+
+				if (isRequest(line.text)) {
+					var request = new vscode.DocumentSymbol(line.text, 'Request', vscode.SymbolKind.Method, line.range, line.range);
+					symbols.push(request);
 				}
 			}
 
 			for (var i = 0; i < symbols.length; i++) {
-				var childs = [];
-				childs = findChildNodes(i, symbols);
-				for (var j = 0; j < childs.length; j++) {
-					symbols[i].children.push(childs[j]);
+				if (isSectionSymbol(symbols[i])) {
+					var childs = [];
+					childs = findNestedSections(i, symbols);
+					for (var j = 0; j < childs.length; j++) {
+						symbols[i].children.push(childs[j]);
+					}
+
+					var requests = [];
+					requests = findRequests(i, symbols);
+					for (var j = 0; j < requests.length; j++) {
+						symbols[i].children.push(requests[j]);
+					}
 				}
 			}
 
 			var roots = [];
 			symbols.forEach(symbol => {
-				if (isRootSymbol(symbol)) {
+				if (isRootSection(symbol)) {
 					roots.push(symbol);
 				}
 			});
